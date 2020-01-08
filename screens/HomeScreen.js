3936072f192db4202a97a1e497ddaa0eea8c9d6a
dixitU <dixitukani2@gmail.com>
2020-01-07 23:57:33 -0800

    first commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -47,7 +47,7 @@ export default function HomeScreen() {
         <View style={styles.helpContainer}>
           <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
             <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
+              knock knock tera baap aaya!!!
             </Text>
           </TouchableOpacity>
         </View>
